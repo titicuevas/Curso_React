24308b995a7885a3b187974bc0238974a55d4c0a
titicuevas <enrique.cuevas@iesdonana.org>
2024-05-27 18:08:03 +0200

    Final

--- project/02-tic-tac-toe/vite.config.js
+++ project/02-tic-tac-toe/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Curso_React/project/02-tic-tac-toe/'
+  base: '/Curso_React/project/02-tic-tac-toe/', 
 })
